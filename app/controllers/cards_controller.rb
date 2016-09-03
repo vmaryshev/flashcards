9714662e712e17619ec32ea3a1e130eac1bcecc8
vladimir <vmaryshev1@gmail.com>
2016-09-03 15:51:49 +0300

    Исправил стилистику

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,15 +1,14 @@
 class CardsController < ApplicationController
-  
   def index
     @cards = Card.all
   end
 
   def new
-  	@card = Card.new
+    @card = Card.new
   end
 
   def create
-  	@card = Card.new(card_params)
+    @card = Card.new(card_params)
     if @card.save
       redirect_to cards_path
     else
@@ -18,26 +17,25 @@ class CardsController < ApplicationController
   end
 
   def edit
-  	@card = Card.find(params[:id])
+    @card = Card.find(params[:id])
   end
 
   def update
-  	@card = Card.find(params[:id])
-  	if @card.update(card_params)
-  	  redirect_to cards_path
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      redirect_to cards_path
     else
-      render :edit 
+      render :edit
     end
   end
 
   def destroy
-  	@card = Card.find(params[:id])
-  	@card.destroy
-  	redirect_to cards_path
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
   end
-  
+
   def card_params
-      params.require(:card).permit(:original_text, :translated_text)
+    params.require(:card).permit(:original_text, :translated_text)
   end
-  
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -6,15 +6,14 @@ class Card < ActiveRecord::Base
 
   before_validation :set_review_date, on: :create
 
-  def check_texts 
+  def check_texts
     if original_text.mb_chars.downcase.strip == translated_text.mb_chars.downcase.strip
-       errors.add(:original_text, "is not original")
-       errors.add(:translated_text, "is not original") 
+      errors.add(:original_text, "similar_of_texts")
+      errors.add(:translated_text, "similar_of_texts")
     end
   end
 
   def set_review_date
-  	self.review_date = 3.days.since
+    self.review_date = 3.days.since
   end
 end
-
